31820a041193f93d09504e854f440a65004bdea6
Laura Gomez <laura@elenagoto.ch>
2025-11-06 14:15:31 +0100

    feat: add footer with render timestamp in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 import './styles.css';
 
-function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
+export const metadata = {
+  title: 'Hello Next JS',
+  description: 'For the Joy of React course',
+};
 
+function RootLayout({ children }) {
+  const timestamp = new Date().toLocaleString();
+  return (
+    <html lang='en'>
+      <body>
+        <header></header>
+        {children}
+        <footer>Page rendered on {timestamp}</footer>
       </body>
     </html>
   );
